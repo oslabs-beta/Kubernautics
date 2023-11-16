0150ac902ad784b2b4567ff16dcfbda1fb54648a
Tyler Shelton <tdshelton@gmail.com>
2023-11-15 21:45:21 -0800

    Comment middleware sections

--- server/server.js
+++ server/server.js
@@ -7,16 +7,17 @@ const dataRoutes = require('./routers/dataRoutes')
 const app = express();
 const PORT = process.env.EXPRESS_PORT || 3000;
 
+// baseline middleware setup
 app.use(cors());
 app.use(express.json());
 
+// application routes
 app.use('/api', dataRoutes);
 
 app.use('/', (req, res) => {
   console.log(req)
   res.sendFile(path.join(__dirname, '../src/index.html'));
 })
-
 
 
 app.listen(PORT, () => {

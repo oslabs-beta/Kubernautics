21861a127c257f6d25f56c1b3daa2da1b4d68595
Tyler Shelton <tdshelton@gmail.com>
2023-11-15 21:46:14 -0800

    Remove unused module

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const axios = require('axios');
 const dataRoutes = require('./routers/dataRoutes')
 
 const app = express();

f932ffe2605d464efaab60656eb37a50cdb25c48
JordanEBuranskas <jordanburanskas@gmail.com>
2023-11-16 15:57:39 -0500

    update to UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { Data } from ".//data";
+import { Pie } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
+
 // import './src/styless.css';
 
 function App () {
+
   return (
     <div id="application">
       {/* <h1>Whole App</h1> */}
@@ -16,7 +24,7 @@ const Main = (props) => {
   return (
     <div id="main">
       <h1>Main Component</h1>
-        <Charts/>
+        <ChartsSection/>
         
   </div>
   )
@@ -26,6 +34,7 @@ const NavBar = (props) => {
 
   return (
     <div className='navBar'>
+      <img src="images.jpg"/> 
       <li></li>
     {/* <li><a href="#">Change Table</a></li> */}
     <li><a href="#">Log Out</a></li>
@@ -37,41 +46,105 @@ const NavBar = (props) => {
 }
 
 
-const Charts = (props) => {
-  return (
-  <div className="charts">
-    <h2>Metrics displays will go here...</h2>
-     <ChartOne/> 
-     <ChartTwo/> 
-     <ChartThree/> 
-   </div>
-  )
-}
+const ChartsSection = (props) => {
+
+//fetch request
+//useEffect
+//options
+//state for both chartData and option
 
 
-const ChartOne = (props) => {
+
+  // const [key, setChartOne] = useState({
+  //   labels: Data.map((data) => data.year), 
+  //   datasets: [
+  //     {
+  //       label: "Users Gained ",
+  //       data: Data.map((data) => data.userGain),
+
+
+  //       backgroundColor: [
+  //         "rgba(61,330,321,1)",
+  //         "rgba(85,190,190,4)",
+  //         "#50AF95",
+  //         "#f3ba2f",
+  //         "#2a71d0"
+  //       ],
+  //       borderColor: "black",
+  //       borderWidth: 2
+  //     }
+  //   ]
+  // });
+ 
   return (
-    <div id='ChartOne'>
-      <h1>The first metric will go here</h1>
+    <div className="Charts">
+      {/* <p>Using Chart.js in React</p> */}
+      <ChartOne chartData={chartData} />
+      <ChartTwo chartData={chartData} />
+      <ChartThree></ChartThree>
+
     </div>
-  )
+  );
 }
 
-const ChartTwo = (props) => {
+const ChartOne = ({ chartData }) => {
+
+  const [chartOneState, setChartOneState] = UseState({
+
+  })
+  data={chartData}
+  //create code to pull out CPU usage
+
+  newdata=
+
   return (
-    <div id='ChartTwo'>
-      <h1>The second metric will go here</h1>
+    <div className="pie-chart">
+      <Pie
+
+        // data={chartData}
+
+
+
+
+        options={{
+          plugins: {
+            refresh: 300
+            title: {
+              display: true,
+              text: "Users Gained between 2016-2020"
+            }
+          }
+        }}
+      />
     </div>
-  )
+  );
 }
 
-const ChartThree = (props) => {
+const ChartTwo = ({ chartData }) => {
   return (
-    <div id='ChartThree'>
-      <h1>The third metric will go here, etc.</h1>
+    <div id="bar-chart">
+      <Bar
+
+        data={chartData}
+
+
+
+        options={{
+          maintainAspectRatio: false,
+          plugins: {
+            title: {
+              display: true,
+              text: "Users Gained between 2016-2020"
+            },
+            legend: {
+              display: false
+            }
+          }
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
 
 

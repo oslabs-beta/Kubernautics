c66c8cb30eaa808f608cd0d300eb836d5300549a
Edward Li <edwardli744@gmail.com>
2023-11-17 12:09:07 -0800

    removing some uncommented code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,32 +13,4 @@ function App() {
   );
 }
 
-// const ChartOne = ({ chartData }) => {
-
-//   const [chartOneState, setChartOneState] = UseState({
-
-//   })
-//   data={chartData}
-//   //create code to pull out CPU usage
-
-//   return (
-//     <div className="pie-chart">
-//       <Pie
-
-//         // data={chartData}
-
-//         options={{
-//           plugins: {
-//             refresh: 300
-//             title: {
-//               display: true,
-//               text: "Users Gained between 2016-2020"
-//             }
-//           }
-//         }}
-//       />
-//     </div>
-//   );
-// }
-
 export default App;

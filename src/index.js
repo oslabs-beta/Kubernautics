a5c37233b005eef01648df4108b8edd1f2188420
Tyler Shelton <tdshelton@gmail.com>
2023-11-20 13:47:06 -0800

    Break dashboard styling out into its own file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-import styles from '../assets/styles.scss';
+import styles from '../assets/styles/styles.scss';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);

6d417a0d1bcb96fd357b19919829345182f7861c
Tyler Shelton <tdshelton@gmail.com>
2023-11-20 13:48:00 -0800

    Clean out comment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import NavBar from './NavBar';
 import Dashboard from './Dashboard';
 
-//effectively will be our home page
-
 function App() {
   return (
     <div id='application'>

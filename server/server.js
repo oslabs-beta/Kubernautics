482afece67550a69aae9e26035da4d044252c0de
Edward Li <edwardli744@gmail.com>
2023-11-20 14:55:30 -0800

    removed the one line from the abandoned service routing method

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 // application routes
 app.use('/api', dataRoutes);
-// app.use('/addMetric', metricRoutes)
 app.get('/', (req, res) => {
   res.status(200).sendFile('src/components/pages/Dashboard/index.html');
 });

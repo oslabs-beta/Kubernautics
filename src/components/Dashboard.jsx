1b8ee6eec51d8ca85a4a453b0ecdfdf22e88d12c
Tyler Shelton <tdshelton@gmail.com>
2023-11-24 10:27:37 -0800

    Set up a space to define default dashboard metrics

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,14 @@ import React, { useState } from 'react';
 import MonitoringComponent from './MonitoringComponent';
 
 const Dashboard = (props) => {
+  // Whatever charts (if any) we want up by default can go here. If we decide ultimately
+  // to present the user a blank slate dashboard to work from, this can be deleted.
+  const defaultCharts = [
+    { query: 'container_cpu_usage_seconds_total{namespace="kubernautics-dev"}[20m]' },
+    { query: 'container_cpu_usage_seconds_total{namespace="kubernautics-dev"}[20m]' },
+    { query: 'container_cpu_usage_seconds_total{namespace="kubernautics-dev"}[20m]' },
+    { query: 'container_cpu_usage_seconds_total{namespace="kubernautics-dev"}[20m]' },  
+  ]
   const [charts, setCharts] = useState(defaultCharts);
 
   return (

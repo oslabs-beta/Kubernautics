615f81c535ec083b438077cea7dced1eb6f5fbc7
Edward Li <edwardli744@gmail.com>
2023-11-27 14:36:47 -0800

    adjust object stored in localstorage to account for stepSize

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,15 +3,11 @@ import MonitoringComponent from './MonitoringComponent';
 import { CustomChart } from './CustomChart';
 // import ClearChart from './ClearCharts';
 const Dashboard = (props) => {
-  // Whatever charts (if any) we want up by default can go here. If we decide ultimately
-  // to present the user a blank slate dashboard to work from, this can be deleted.
-
-  //Haven't added but might want a way to delete charts from the dashboard. as simple as locating
-  //and deleting the query line from localStorage
   const defaultCharts = [
     {
       query: 'container_cpu_usage_seconds_total{namespace="kubernautics-dev"}',
       range: '20m',
+      stepSize: 80,
     },
   ];
   const [charts, setCharts] = useState(() => {
@@ -27,12 +23,15 @@ const Dashboard = (props) => {
     }
   });
 
-  const SaveDataToLocalStorage = (query, range) => {
+  const SaveDataToLocalStorage = (query, range, stepSize) => {
     //Get the existing Data from localStorage
     const existingData = JSON.parse(localStorage.getItem('session')) || [];
     //Update the existing storage with the data passed through
     console.log('existingData', existingData);
-    const newData = [...existingData, { query: query, range: range }];
+    const newData = [
+      ...existingData,
+      { query: query, range: range, stepSize: stepSize },
+    ];
     //Rewrite the localstorage
     localStorage.setItem('session', JSON.stringify(newData));
     setCharts(JSON.parse(localStorage.getItem('session')));
@@ -49,6 +48,7 @@ const Dashboard = (props) => {
               key={`chart-${i}`}
               query={chart.query}
               range={chart.range}
+              stepSize={chart.stepSize}
             />
           );
         })}

ecb0fef4ee8127c40f8b55634db5111cdb0b69e6
Edward Li <edwardli744@gmail.com>
2023-11-29 12:58:11 -0800

    removed deprecated route

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import nautics from '../../assets/images/nautics';
+import nautics from '../../assets/images/nautics';
 
 const NavBar = () => {
   return (
     <header className='navBar'>
       <div className='brand'>
-        {/* <img src={nautics} className='logo' /> */}
+        <img src={nautics} className='logo' />
         <h1>Kubernautics</h1>
       </div>
 
@@ -15,7 +15,6 @@ const NavBar = () => {
           <li>
             <Link to='/'>Home</Link>
           </li>
-          {/* <li><Link to='/addMetric'>Add Metric</Link></li> */}
           <li>
             <Link to='/clusterVisualizer'>Visualizer</Link>
           </li>
